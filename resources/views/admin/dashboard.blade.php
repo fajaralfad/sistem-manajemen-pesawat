b6971a78cf07c7465ac969664e57eb60847742a0
fajaralfad <fajaralfad85@gmail.com>
2024-11-03 23:04:39 +0700

    mengembalikan data count

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -37,7 +37,6 @@
 @endsection
 
 @section('content')
-
 <div class="d-flex">
     <div class="content w-100">
         <div class="container mt-4">
@@ -47,17 +46,15 @@
                 <div class="card m-4 text-white bg-primary" style="width: 23rem;">
                     <div class="card-body">
                         <h5 class="card-title"><b>Active Technician</b></h5>
-                        
                         <i data-feather="users" width="98" height="98"></i>
-                        <span class="display-5 ms-3" style="font-weight:bold;">{{ $teknisiCount }}</span>
+                        <h2>{{ $activeTechnicians }}</h2>
                     </div>
                 </div>
                 <div class="card m-4 text-white bg-warning" style="width: 23rem;">
                     <div class="card-body">
                         <h5 class="card-title"><b>Active Manager</b></h5>
                         <i data-feather="user" width="98" height="98"></i>
-                        <span class="display-4 ms-3" style="font-weight:bold;">{{$managerCount}}</span>
-
+                        <h2>{{ $activeManagers }}</h2>
                     </div>
                 </div>
             </div>
